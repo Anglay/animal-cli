782f15c63d10cb9cdbd8dfcef04eadfc582d6a5e
Anglay <435641688@qq.com>
2021-02-03 17:18:01 +0800

    脚本调整

--- cli.js
+++ cli.js
@@ -51,7 +51,7 @@ inquirer.prompt([
     console.log(answers)
     const spinner = ora("Downloading...");
     spinner.start();
-    download('direct:https://github.com/472756921/Brts-ts', 'test', { clone: true }, function (err) {
+    download('direct:https://github.com/Anglay/animal-cli', 'test', { clone: true }, function (err) {
         if (err) {
             spinner.fail();
             console.log(err)
